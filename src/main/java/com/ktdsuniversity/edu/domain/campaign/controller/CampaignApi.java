ba85098a1bf89470e8df558b8f7ae73099899b34
sim748 <sim748@naver.com>
2025-12-08 17:53:24 +0900

    포스트 승인api까지 만듬

--- src/main/java/com/ktdsuniversity/edu/domain/campaign/controller/CampaignApi.java
+++ src/main/java/com/ktdsuniversity/edu/domain/campaign/controller/CampaignApi.java
@@ -1,5 +1,6 @@
 package com.ktdsuniversity.edu.domain.campaign.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -9,15 +10,20 @@ import org.springframework.security.core.Authentication;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.ktdsuniversity.edu.domain.campaign.service.CampaignService;
 import com.ktdsuniversity.edu.domain.campaign.vo.request.RequestApplicantVO;
 import com.ktdsuniversity.edu.domain.campaign.vo.request.RequestSearchCampaignVO;
+import com.ktdsuniversity.edu.domain.campaign.vo.response.ResponseAdoptListVO;
 import com.ktdsuniversity.edu.domain.campaign.vo.response.ResponseApplicantListVO;
 import com.ktdsuniversity.edu.domain.campaign.vo.response.ResponseCampaignListVO;
+import com.ktdsuniversity.edu.domain.campaign.vo.response.ResponseDenyHistoryVO;
 import com.ktdsuniversity.edu.domain.user.vo.UserVO;
 import com.ktdsuniversity.edu.global.common.AjaxResponse;
 import com.ktdsuniversity.edu.global.exceptions.BrichException;
@@ -31,13 +37,11 @@ public class CampaignApi {
 	private CampaignService campaignService;
 
 	// 석진 ===============================================================
-	@GetMapping("list/{id}")
+	@GetMapping("list")
 	public AjaxResponse readCampaignList(Authentication authentication,
-										 @PathVariable String id,
 										 RequestSearchCampaignVO requestSearchCampaignVO) {
 		requestSearchCampaignVO.setListSize(8);
 		requestSearchCampaignVO.setPageCountInGroup(5);
-		requestSearchCampaignVO.setLoginId(id);
 		
 		
 		ResponseCampaignListVO campaignList = this.campaignService.readCampaignListByUsrId(requestSearchCampaignVO);
@@ -50,13 +54,11 @@ public class CampaignApi {
 		return ajaxResponse;
 	}
 	
-	@GetMapping("/applicant/{cmpnId}")
-    public AjaxResponse readApplicantList(@PathVariable String cmpnId,
-    									  RequestApplicantVO requestApplicantVO,
+	@GetMapping("/applicant")
+    public AjaxResponse readApplicantList(RequestApplicantVO requestApplicantVO,
     									  Authentication authentication) {
     	requestApplicantVO.setListSize(10);
     	requestApplicantVO.setPageCountInGroup(10);
-    	requestApplicantVO.setCmpnId(cmpnId);
 
     	if (requestApplicantVO.getOrder() != null) {
     		requestApplicantVO.setOrder(requestApplicantVO.getOrder().toUpperCase());
@@ -70,5 +72,66 @@ public class CampaignApi {
     	
     	log.info("applicant : ", applicantList);
     	return ajaxResponse;
+    }
+	
+	@PutMapping("/adoptChange")
+    @ResponseBody
+    public boolean doUpdateAdptYnAction(@RequestBody RequestApplicantVO requestApplicantVO,
+    									Authentication authentication) {
+//    	requestApplicantVO.setUsrId(loginUser.getUsrId());
+    	boolean update = this.campaignService.updateAdptYnByCmpnPstAdptId(requestApplicantVO);
+    	
+    	if (update) {
+    		return true;
+    	}
+    	
+    	else {
+    		return false;
+    	}
+    }
+	
+	@GetMapping("/adopt")
+    public AjaxResponse readAdoptList(RequestApplicantVO requestApplicantVO,
+    								  Authentication authentication) {
+    	requestApplicantVO.setListSize(10);
+    	requestApplicantVO.setPageCountInGroup(10);
+    			
+    	ResponseAdoptListVO adoptList = this.campaignService.readResponseAdoptListByCmpnId(requestApplicantVO);
+    	
+    	AjaxResponse ajaxResponse = new AjaxResponse();
+    	ajaxResponse.setBody(adoptList);
+    	ajaxResponse.setPaginator(requestApplicantVO);
+    	
+    	log.info("adoptList---:" + adoptList);
+    	return ajaxResponse;
+    }
+	
+	@GetMapping("/post-deny-history/{cmpnPstAdptId}")
+	@ResponseBody
+	public AjaxResponse doReadDenyHistoryAction(@PathVariable String cmpnPstAdptId) {
+		List<ResponseDenyHistoryVO> history = this.campaignService.readDenyHistoryByCmpnPstAdptId(cmpnPstAdptId);
+
+		AjaxResponse response = new AjaxResponse();
+		response.setBody(history);
+		
+		return response;
+	}
+	
+	@PutMapping("/postapprove/{cmpnPstAdoptId}")
+    @ResponseBody
+    public boolean doUpdatePstSttsApproveAction(@PathVariable String cmpnPstAdoptId,
+    											Authentication authentication) {
+    	RequestApplicantVO requestApplicantVO = new RequestApplicantVO();
+    	requestApplicantVO.setCmpnPstAdptId(cmpnPstAdoptId);
+//    	requestApplicantVO.setUsrId(loginUser.getUsrId());	authentication.getPrincipal()로 가져올 것
+    	requestApplicantVO.setUsrId("USR-20240413-000007");
+    	boolean update = this.campaignService.updatePstSttsApproveByCmpnPstAdoptId(requestApplicantVO);
+    	
+    	if (update) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
     }
 }
