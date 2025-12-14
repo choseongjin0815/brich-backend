8691879603f06a168d824eb76bd0d9d9c2012100
choseongin0815 <chobocho0815@gmail.com>
2025-12-14 19:10:57 +0900

    feat: 계정관리 베이스 세팅 구현

--- src/main/java/com/ktdsuniversity/edu/domain/user/controller/UserApi.java
+++ src/main/java/com/ktdsuniversity/edu/domain/user/controller/UserApi.java
@@ -1,10 +1,15 @@
 package com.ktdsuniversity.edu.domain.user.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,14 +17,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.ktdsuniversity.edu.domain.campaign.service.CampaignService;
 import com.ktdsuniversity.edu.domain.campaign.vo.response.ResponseCampaignwriteVO;
 import com.ktdsuniversity.edu.domain.user.service.UserService;
+import com.ktdsuniversity.edu.domain.user.vo.UserVO;
+import com.ktdsuniversity.edu.domain.user.vo.request.RequestUserAccountPasswordVO;
 import com.ktdsuniversity.edu.domain.user.vo.request.RequestUserFindIdVO;
+import com.ktdsuniversity.edu.domain.user.vo.request.RequestUserInfoModifyVO;
 import com.ktdsuniversity.edu.domain.user.vo.request.RequestUserRegistVO;
 import com.ktdsuniversity.edu.domain.user.vo.request.RequestUserResetPasswordVO;
+import com.ktdsuniversity.edu.domain.user.vo.response.ResponseUserInfoVO;
+import com.ktdsuniversity.edu.domain.user.vo.response.ResponseUserSubscriptionInfoVO;
 import com.ktdsuniversity.edu.global.common.AjaxResponse;
+import com.ktdsuniversity.edu.global.common.CommonCodeVO;
 import com.ktdsuniversity.edu.global.util.AuthenticationUtil;
 
 import jakarta.validation.Valid;
@@ -133,4 +145,108 @@ public class UserApi {
 		
 		return ajaxResponse;
 	}
+	
+	@GetMapping("/account")
+	public AjaxResponse getAccountInfo() {
+		AjaxResponse ajaxResponse = new AjaxResponse();
+		String usrId = AuthenticationUtil.getUserVO().getUsrId();
+		
+		ResponseUserInfoVO userInfo = this.userService.readUserByUserId(usrId);
+		ResponseCampaignwriteVO common = userInfo.getResponseCampaignwriteVO();
+		
+		ajaxResponse.setBody(Map.of("userInfo", userInfo, "common", common));
+		
+		return ajaxResponse;
+	}
+	
+	//TODO 필요할지 체크 필요
+	@GetMapping("/account/{cdId}")
+	public AjaxResponse getDistrictAction(@PathVariable String cdId) {
+		AjaxResponse response = new AjaxResponse();
+		
+		List<CommonCodeVO> districtList = this.campaignService.readDistrictByCdId(cdId);
+		response.setBody(districtList);
+		return response;
+	}
+	
+	/*
+	 * 계정 정보 수정 
+	 */
+	@PostMapping("/acoount")
+	public AjaxResponse doUpdateUserInfo(RequestUserInfoModifyVO requestUserInfoModifyVO) {
+		String usrId = AuthenticationUtil.getUserVO().getUsrId();
+		requestUserInfoModifyVO.setUsrId(usrId);
+		boolean updateResult = this.userService.updateUserInfoByUsrId(requestUserInfoModifyVO);
+		AjaxResponse ajaxResponse = new AjaxResponse();
+		
+		if(updateResult == false) {
+			ajaxResponse.setBody("수정에 실패했습니다.");
+			return ajaxResponse;
+		}
+		ajaxResponse.setBody("수정에 성공했습니다.");
+		
+		return ajaxResponse;
+	}
+	
+	/**
+	 * 비밀번호 재설정 요청
+	 */
+	@PostMapping("/reset-password")
+	public AjaxResponse resetPassword(RequestUserAccountPasswordVO requestUserAccountPasswordVO) {
+		
+		String usrId = AuthenticationUtil.getUserVO().getUsrId();
+		requestUserAccountPasswordVO.setUsrId(usrId);
+		log.info("{}", requestUserAccountPasswordVO);
+		
+		AjaxResponse ajaxResponse = new AjaxResponse();
+		
+		if(requestUserAccountPasswordVO.getCurrentPswrd().equals("")|| 
+		   requestUserAccountPasswordVO.getNewPswrd().equals("") ||
+		   requestUserAccountPasswordVO.getCurrentPswrd().equals("")) {
+		   ajaxResponse.setBody(Map.of("result", "모든 값을 입력해야합니다."));
+    	   return ajaxResponse;
+		} 
+		
+		if (!requestUserAccountPasswordVO.getNewPswrd().matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d!@#$%^&*]{8,16}$")) {
+    		ajaxResponse.setBody(Map.of("result", "비밀번호는 8~16자까지 가능하며 영문자와 숫자를 혼합해야합니다."));
+    		return ajaxResponse;
+    	} 
+		
+		if(!requestUserAccountPasswordVO.getNewPswrd().equals(requestUserAccountPasswordVO.getNewPswrdConfirm())) {
+			ajaxResponse.setBody(Map.of("result", "비밀번호가 일치하지 않습니다."));
+			return ajaxResponse;
+		}
+		
+		boolean updatePassword = this.userService.updatePswrdByUsrId(requestUserAccountPasswordVO);
+		
+		//입력한 기존 비밀번호가 DB에 저장된 비밀번호와 같지 않은 경우
+		if(updatePassword == false) {
+			ajaxResponse.setBody(Map.of("result", "현재 비밀번호가 일치하지 않습니다."));
+			return ajaxResponse;
+		}
+		
+		ajaxResponse.setBody(Map.of("result", "비밀번호가 재설정 되었습니다!"));
+		
+		
+		return ajaxResponse;
+	}
+	
+	/**
+	 * 구독 정보
+	 */
+	@GetMapping("/subscription-info") 
+	public AjaxResponse getSubscriptionInfo() {
+		String usrId = AuthenticationUtil.getUserVO().getUsrId();
+		AjaxResponse ajaxResponse = new AjaxResponse();
+	
+
+		ResponseUserSubscriptionInfoVO subscriptionInfo = this.userService.readSubscriptionInfoByUserId(usrId);
+		log.info("subInfo:  {}", subscriptionInfo);
+		ajaxResponse.setBody(subscriptionInfo);
+	
+		
+		return ajaxResponse;
+	}
+	
+	
 }
